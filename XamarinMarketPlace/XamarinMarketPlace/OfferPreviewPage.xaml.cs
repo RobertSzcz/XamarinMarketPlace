d8b21800a1a3b0a8939a7b086cb2ed6ae78e1445
Joni Koivula <joni.koivula@student.hamk.fi>
2017-10-13 08:52:57 +0300

    Phonecalling

--- XamarinMarketPlace/XamarinMarketPlace/OfferPreviewPage.xaml.cs
+++ XamarinMarketPlace/XamarinMarketPlace/OfferPreviewPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using System.Diagnostics;
 
 namespace XamarinMarketPlace
 {
@@ -18,6 +19,27 @@ namespace XamarinMarketPlace
 
             offerImage.Source = FileImageSource.FromUri(
                 new Uri("http://www.123mobilewallpapers.com/wp-content/uploads/2014/09/snow_on_hill.jpg"));
+        }
+
+        async void Call_Clicked(object sender, EventArgs e)
+        {
+            if (await this.DisplayAlert(
+                    "Dial a Number",
+                    "Would you like to call " + phonenumber.Text + "?",
+                    "Yes",
+                    "No"))
+            {
+                var dialer = DependencyService.Get<IDialer>();
+
+                try {
+                    if (dialer != null)
+                    {
+                        //App.PhoneNumbers.Add(phonenumber.Text);
+                        dialer.Dial(phonenumber.Text);
+                    }
+                } catch (Exception err) { Debug.WriteLine(err); }
+
+            }
         }
     }
 }
